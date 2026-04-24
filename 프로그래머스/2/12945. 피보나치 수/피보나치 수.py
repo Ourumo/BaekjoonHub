334449fce81ebf5bc0138b1ab7a4682642dec94b
Ourumo <133006490+Ourumo@users.noreply.github.com>
2026-04-24 10:08:12 +0900

    [level 2] Title: 피보나치 수, Time: 517.10 ms, Memory: 456 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -1,15 +1,22 @@
 import sys
-sys.setrecursionlimit(200000)
+sys.setrecursionlimit(10**6)
 
-def fibonaci(n, arr):
-    if n <= 1:
-        return n
-    if arr.get(n):
-        return arr[n]
-    temp = fibonaci(n - 1, arr) + fibonaci(n - 2, arr)
-    arr[n] = temp
-    return temp
+# def fibonaci(n, arr):
+#     if n <= 1:
+#         return n
+#     if arr.get(n):
+#         return arr[n]
+#     temp = fibonaci(n - 1, arr) + fibonaci(n - 2, arr)
+#     arr[n] = temp
+#     return temp
 
 def solution(n):
-    arr = {}
-    return fibonaci(n, arr) % 1234567
+    # arr = {}
+    # return fibonaci(n, arr) % 1234567
+    arr = []
+    for i in range(n + 1):
+        if i <= 1:
+            arr.append(i)
+        else:
+            arr.append(arr[i - 1] + arr[i - 2])
+    return arr[n] % 1234567
