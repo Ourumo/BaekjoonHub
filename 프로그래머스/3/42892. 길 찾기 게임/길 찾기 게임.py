bb87b661991259e21f17dcea005ae4b824bcc40b
Ourumo <133006490+Ourumo@users.noreply.github.com>
2026-04-27 14:31:14 +0900

    [level 3] Title: 길 찾기 게임, Time: 323.02 ms, Memory: 13.8 MB -BaekjoonHub

--- 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.py
+++ 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.py
@@ -1,3 +1,5 @@
+# 리팩토링 1. 좌측 우측에 노드 추가를 Node 클래스의 메서드로 변환
+
 import sys
 sys.setrecursionlimit(10**6)
 
@@ -10,19 +12,34 @@ class Node:
         self.right_child = None
         self.is_root = is_root
     
-    def preFix(self, arr):
-        if self.left_child:
-            self.left_child.preFix(arr)
-        if self.right_child:
-            self.right_child.preFix(arr)
-        arr.append(self.idx)
-    
     def postFix(self, arr):
-        arr.append(self.idx)
         if self.left_child:
             self.left_child.postFix(arr)
         if self.right_child:
             self.right_child.postFix(arr)
+        arr.append(self.idx)
+    
+    def preFix(self, arr):
+        arr.append(self.idx)
+        if self.left_child:
+            self.left_child.preFix(arr)
+        if self.right_child:
+            self.right_child.preFix(arr)
+    
+    # 리팩토링 1
+    def insert(self, node):
+        # node 노드가 현재 노드의 좌측 자식인지 우측 자식인지 확인
+        direction = True if self.x < node.x else False
+        if direction:
+            if self.right_child:
+                self.right_child.insert(node)
+            else:
+                self.right_child = node
+        else:
+            if self.left_child:
+                self.left_child.insert(node)
+            else:
+                self.left_child = node
 
 def solution(nodeinfo):
     # nodeinfo = [[x, y, idx], ...]
@@ -35,40 +52,18 @@ def solution(nodeinfo):
     # Node 클래스로 형식 변환
     nodes = [Node(nodeinfo[i], True if i == 0 else False) for i in range(len(nodeinfo))]
 
-    root = None
+    root = nodes[0]
     for n in nodes:
         # 루트 노드는 건너뛰기
         if n.is_root == True:
             root = n
             continue
-        # n 노드가 루트 노드의 좌측 자식인지 우측 자식인지 확인
-        parent = root.right_child if root.x < n.x else root.left_child
-        direction = True if root.x < n.x else False
-        # 루트의 좌측 혹은 우측 자식 노드가 비어있다면 그 곳에 추가
-        if not parent:
-            if direction:
-                root.right_child = n
-            else:
-                root.left_child = n
-            continue
-        # 루트의 좌측 혹은 우측 자식 노드가 비어있지 않다면 더 깊게 접근
-        is_repeat = True
-        while is_repeat:
-            tmp_parent = parent.right_child if parent.x < n.x else parent.left_child 
-            if tmp_parent:
-                parent = tmp_parent
-            else:
-                direction = True if parent.x < n.x else False
-                is_repeat = False
-        # 부모의 좌측 혹은 우측 자식 노드가 비어있다면 그 곳에 추가
-        if direction:
-            parent.right_child = n
-        else:
-            parent.left_child = n
+        # 리팩토링 1
+        root.insert(n)
 
     # 전위 순회, 후위 순회
     preorder, postorder = [], []
     root.preFix(preorder)
     root.postFix(postorder)
     
-    return [postorder, preorder]
+    return [preorder, postorder]
