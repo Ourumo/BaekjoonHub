6e4bcdb2cfcdff272568706903fd7a9e9045ed13
Ourumo <133006490+Ourumo@users.noreply.github.com>
2026-05-12 18:31:07 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.11 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.py
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.py
@@ -1,8 +1,12 @@
 def solution(my_string, s, e):
-    arr = list(map(str, my_string))
-    reverse_split_arr = arr[s:e + 1][::-1]
+#     arr = list(map(str, my_string))
+#     reverse_split_arr = arr[s:e + 1][::-1]
     
-    for i in range(e - s + 1):
-        arr[s + i] = reverse_split_arr[i]
+#     for i in range(e - s + 1):
+#         arr[s + i] = reverse_split_arr[i]
 
-    return "".join(arr)
+#     return "".join(arr)
+    arr = list(map(str, my_string))
+    arr1, arr2, arr3 = arr[0:s], arr[s:e + 1][::-1], arr[e + 1:len(arr)]
+    
+    return "".join(arr1 + arr2 + arr3)
