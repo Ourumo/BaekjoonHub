5fa62186289598ce4104d25d3f817b15cc824c44
Ourumo <133006490+Ourumo@users.noreply.github.com>
2026-05-17 23:13:06 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.76 ms, Memory: 14.6 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.py
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.py
@@ -1,4 +1,6 @@
 def solution(arr):
-    min_v = min(arr)
-    new_arr = list(filter(lambda x: x != min_v, arr))
-    return new_arr if new_arr else [-1]
+    # min_v = min(arr)
+    # new_arr = list(filter(lambda x: x != min_v, arr))
+    # return new_arr if new_arr else [-1]
+    arr.remove(min(arr))
+    return arr if arr else [-1]
